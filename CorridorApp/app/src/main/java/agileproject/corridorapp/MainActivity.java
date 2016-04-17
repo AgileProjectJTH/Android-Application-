069456baa56387aa445f6e16f6087992de13a1be
Hansrutger <bloody_pirate_4-ever@hotmail.com>
2016-04-17 17:35:59 +0200

    Changed MainActivity
    Coded by Mikael
    
    Added:
    - DatePicker
    - SetTimer
    - Buttons
    - Information

--- CorridorApp/app/src/main/java/agileproject/corridorapp/MainActivity.java
+++ CorridorApp/app/src/main/java/agileproject/corridorapp/MainActivity.java
@@ -9,8 +9,23 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button availabilityBtn;
+    Button setTimeBtn;
+    Button setDateBtn;
+    TextView availabilityText;
+    RelativeLayout lLayout;
+    CheckBox availableCheckBox;
+    DatePicker unavailableDatePicker;
+
+    boolean IsAvailable = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +33,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        availabilityBtn = (Button)findViewById(R.id.availabilityBtn);
+        setTimeBtn = (Button)findViewById(R.id.setTimeBtn);
+        setDateBtn = (Button)findViewById(R.id.setDateBtn);
+
+        availabilityText = (TextView)findViewById(R.id.availabilityText);
+
+        availableCheckBox = (CheckBox)findViewById(R.id.availableCheckBox);
+        unavailableDatePicker = (DatePicker)findViewById(R.id.unavailableDatePicker);
+
+        lLayout = (RelativeLayout)findViewById(R.id.main_layout);
+        if(IsAvailable)
+        {
+            availabilityBtn.setText(R.string.unavailable_activity_main);
+            availabilityBtn.setBackgroundColor(getResources().getColor(R.color.colorBtnUnavailable));
+            availabilityText.setText(R.string.available_text_activity_main);
+            lLayout.setBackgroundColor((getResources().getColor(R.color.colorAvailable)));
+
+            availableCheckBox.setVisibility(View.INVISIBLE);
+            unavailableDatePicker.setVisibility(View.VISIBLE);
+
+            setTimeBtn.setVisibility(View.INVISIBLE);
+            setDateBtn.setVisibility(View.VISIBLE);
+
+            IsAvailable = false;
+        }
+        else
+        {
+            availabilityBtn.setText(R.string.available_activity_main);
+            availabilityBtn.setBackgroundColor(getResources().getColor(R.color.colorBtnAvailable));
+            availabilityText.setText(R.string.unavailable_text_activity_main);
+            lLayout.setBackgroundColor((getResources().getColor(R.color.colorUnavailable)));
+
+            availableCheckBox.setVisibility(View.VISIBLE);
+            unavailableDatePicker.setVisibility(View.INVISIBLE);
+
+            setTimeBtn.setVisibility(View.VISIBLE);
+            setDateBtn.setVisibility(View.INVISIBLE);
+        }
     }
 
     @Override
@@ -46,5 +100,42 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void availabilityBtn_OnClick(View v){
+        if(IsAvailable)
+        {
+            availabilityBtn.setText(R.string.unavailable_activity_main);
+            availabilityBtn.setBackgroundColor(getResources().getColor(R.color.colorBtnUnavailable));
+            availabilityText.setText(R.string.available_text_activity_main);
+            lLayout.setBackgroundColor((getResources().getColor(R.color.colorAvailable)));
+
+            availableCheckBox.setVisibility(View.INVISIBLE);
+            unavailableDatePicker.setVisibility(View.VISIBLE);
+
+            setTimeBtn.setVisibility(View.INVISIBLE);
+            setDateBtn.setVisibility(View.VISIBLE);
+
+            IsAvailable = false;
+        }
+        else
+        {
+            availabilityBtn.setText(R.string.available_activity_main);
+            availabilityBtn.setBackgroundColor(getResources().getColor(R.color.colorBtnAvailable));
+            availabilityText.setText(R.string.unavailable_text_activity_main);
+            lLayout.setBackgroundColor((getResources().getColor(R.color.colorUnavailable)));
+
+            availableCheckBox.setVisibility(View.VISIBLE);
+            unavailableDatePicker.setVisibility(View.INVISIBLE);
+
+            setTimeBtn.setVisibility(View.VISIBLE);
+            setDateBtn.setVisibility(View.INVISIBLE);
+
+            IsAvailable = true;
+        }
+    }
+
+    public void setSettingsBtn_OnClick(View v){
+
     }
 }
